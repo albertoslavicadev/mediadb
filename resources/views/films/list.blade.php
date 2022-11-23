318c06908722b92e6586e87fad47f11d5b1745af
Alberto Slavica <alberto.slavica@ecit.it>
2022-11-23 16:09:00 +0100

    test

--- resources/views/films/list.blade.php
+++ resources/views/films/list.blade.php
@@ -13,6 +13,7 @@
             <th>Attori Protagonisti</th>
             <th>Generi</th>
             <th>Tags</th>
+            <th>Voto</th>
 
             </thead>
             <tbody>
@@ -50,3 +51,56 @@
         </table>
     </div>
 @endsection
+
+
+<style>
+    div.stars {
+        display: inline-block;
+    }
+
+    input.star { display: none; }
+
+    label.star {
+        float: right;
+        padding: 10px;
+        font-size: 20px;
+        color: #444;
+        transition: all .2s;
+    }
+
+    input.star:checked ~ label.star:before {
+        content: 'f005';
+        color: #e74c3c;
+        transition: all .25s;
+    }
+
+    input.star-5:checked ~ label.star:before {
+        color: #e74c3c;
+        text-shadow: 0 0 5px #7f8c8d;
+    }
+
+    input.star-1:checked ~ label.star:before { color: #F62; }
+
+    label.star:hover { transform: rotate(-15deg) scale(1.3); }
+
+    label.star:before {
+        content: 'f006';
+        font-family: FontAwesome;
+    }
+
+
+    .horline > li:not(:last-child):after {
+        content: " |";
+    }
+    .horline > li {
+        font-weight: bold;
+        color: #ff7e1a;
+
+    }
+</style>
+
+<script>
+    $('#addStar').change('.star', function(e) {
+        $(this).submit();
+    });
+</script>
