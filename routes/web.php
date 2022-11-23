a2c367cf767e69117f92e5eb66a9a3a0e8dc72a3
albertoslavicadev <albertoslavica19@gmail.com>
2022-11-23 18:01:56 +0100

    Devo andare a casa

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,10 @@ use App\Http\Controllers\ActorController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\FilmController::class, 'index'])->name('film');
 
 Route::resource('films', 'FilmController');
 Route::resource('actors', 'ActorController');
